0b4e75de48f402339624b05c0d86e9286ba894cc
Shruti Verma <shrutiverma3105@gmail.com>
2021-05-07 13:29:31 +0530

    commit

---
+++ lib/src/main/java/JavaDS/MyQueue.java
@@ -0,0 +1,24 @@
+package JavaDS;
+
+public class MyQueue {
+    private final MyLinkedList myLinkedList;
+
+    public MyQueue() {
+        this.myLinkedList = new MyLinkedList();
+    }
+    public INode peak(){
+        return myLinkedList.head;
+    }
+    public void enqueue(INode myNode) {
+        if(myLinkedList.head == null){
+            myLinkedList.append(myNode);
+        }
+        else {
+            myLinkedList.append(myNode);
+        }
+    }
+
+    public INode dequeue() {
+        return myLinkedList.popOut();
+    }
+}
---
+++ lib/src/main/java/JavaDS/MyStack.java
@@ -0,0 +1,21 @@
+package JavaDS;
+
+public class MyStack {
+    private final MyLinkedList myLinkedList;
+
+    public MyStack() {
+        this.myLinkedList = new MyLinkedList();
+    }
+
+    public void push(INode myNode) {
+        myLinkedList.add(myNode);
+    }
+
+    public INode peak() {
+        return myLinkedList.head;
+    }
+
+    public INode pop() {
+        return myLinkedList.popOut();
+    }
+}
